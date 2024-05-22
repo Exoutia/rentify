b35529bfba320666a1b18574d398c3f450e7074a
exoutia <jhav365@gmail.com>
2024-05-22 21:51:45 +0530

    adding shadcn-ui

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,9 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "./ui/button";
 
 export default function Root() {
   return (
-    <div>
+    <>
+    <header>
+      <nav>
+          <Button>Button</Button>
+        
+          
+      </nav>
+    </header>
+    <div className="main">
       <Outlet />
     </div>
+    </>
   );
 }
